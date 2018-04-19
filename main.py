9f85b7074c43b94dc0a9e6b599d6a10ce8534426
Edwan Vi <edwan.vi@gmail.com>
2018-04-19 09:49:36 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot('[] ', None, "A bot created for team 3494.", True)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+token = None
+with open("creds.txt") as creds:
+    token = creds.read().strip()
+
+bot.run(token)
