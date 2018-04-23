4055e04d559127fa13ab98cc26525438d20c7f24
Edwan Vi <edwan.vi@gmail.com>
2018-04-23 11:13:50 -0400

    Add footer to embeds correctly and add quotes[] to boiler.py

--- boiler.py
+++ boiler.py
@@ -3,9 +3,14 @@ Util functions. Cuts down on boilerplate code, ideally.
 """
 
 import discord
+import json
+import random
+
+quotes = []
+with open("footers.json") as f:
+    quotes = json.load(f)
 
 def embed_template():
-    em = discord.Embed()
-    em.set_footer(icon_url="https://i.imgur.com/j1bnW3q.png")
+    em = discord.Embed(title="please change this title").set_footer(text=random.choice(quotes), icon_url="https://i.imgur.com/j1bnW3q.png")
     em.colour = discord.Colour.gold()
     return em
