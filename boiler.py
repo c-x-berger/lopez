02420607698c095d172d64ac91686135d53bbedb
Edwan Vi <edwan.vi@gmail.com>
2018-04-23 15:57:38 -0400

    [⚗}] Change icon on Lopez's footer

--- boiler.py
+++ boiler.py
@@ -11,6 +11,6 @@ with open("footers.json") as f:
     quotes = json.load(f)
 
 def embed_template():
-    em = discord.Embed(title="please change this title").set_footer(text=random.choice(quotes), icon_url="https://i.imgur.com/j1bnW3q.png")
+    em = discord.Embed(title="please change this title").set_footer(text=random.choice(quotes), icon_url="https://i.imgur.com/2VepakW.png")
     em.colour = discord.Colour.gold()
     return em
