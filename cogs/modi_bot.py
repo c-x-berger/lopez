90b9952c837b0afab515bdb4b330dec5802cd330
Edwan Vi <edwan.vi@gmail.com>
2018-05-09 14:51:25 -0400

    Fix loading error caused by moving cogs into cogs/

--- cogs/modi_bot.py
+++ cogs/modi_bot.py
@@ -7,39 +7,32 @@ class modi():
         self.bot = bot
         self.special_cogs = special_cogs
 
-    @commands.group(pass_context=True)
+    @commands.group(pass_context=True, description="Base command for module tinkering.\nMust be invoked with a subcommand. Can only be invoked by the bot's creator.")
     async def mod(self, ctx):
-        '''
-        Base command for all module tinkering.
-        '''
+        '''Base command for all module tinkering.'''
         if (ctx.invoked_subcommand is None):
             await self.bot.say("This command must be invoked with a subcommand (`unload`, `load`, or `reload`)!")
 
     @mod.command(pass_context=True)
     async def load(self, ctx, module: str):
-        '''
-        Load a module.
-        '''
+        '''Load a module.'''
         if (ctx.message.author.id == "164342765394591744" and module not in self.special_cogs):
+            module = "cogs." + module
             self.bot.load_extension(module)
             await self.bot.say("Loaded `{}`".format(module))
 
     @mod.command(pass_context=True)
     async def unload(self, ctx, module: str):
-        '''
-        Unload a module.
-        '''
+        '''Unload a module.'''
         if (ctx.message.author.id == "164342765394591744" and module not in self.special_cogs):
             self.bot.unload_extension(module)
             await self.bot.say("Unloaded `{}`".format(module))
 
     @mod.command(pass_context=True)
     async def reload(self, ctx, module: str):
-        '''
-        Reload a module.
-        '''
+        '''Reload a module.'''
         await ctx.invoke(self.unload, module)
-        await ctx.invoke(self.load, module)
+        await ctx.invoke(self.load, "cogs." + module)
 
 
 def setup(bot: commands.Bot):
