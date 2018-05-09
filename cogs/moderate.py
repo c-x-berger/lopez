dafab0bc7b8f94d0c6a7feb645d3c17b932f99e4
Edwan Vi <edwan.vi@gmail.com>
2018-05-09 13:42:18 -0400

    Fix [] purge footer and tweak algo a wee bit

--- cogs/moderate.py
+++ cogs/moderate.py
@@ -9,24 +9,26 @@ class moderate():
 
     @commands.command(description="Enables a user with Manage Messages to bulk delete the last `amount` messages.", pass_context=True)
     async def purge(self, ctx, amount: int):
-        '''
-        Bulk remove messages.
-        '''
+        '''Bulk remove messages.'''
         if (ctx.message.channel.permissions_for(ctx.message.author).manage_messages):
             i = 0
             j = -1
+            messages = list(self.bot.messages)
             while (i < amount + 1):
-                if (list(self.bot.messages)[j].channel.id == ctx.message.channel.id):
-                    await self.bot.delete_message(self.bot.messages[j])
-                    self.bot.messages.remove(self.bot.messages[j])
+                if (messages[j].channel.id == ctx.message.channel.id):
+                    await self.bot.delete_message(messages[j])
                     j = -1
                     i += 1
                 else:
                     j -= 1
             em = boiler.embed_template()
-            em.title = "Purged {} messages".format(amount)
-            em.set_footer(text="Requested by {}".format(
-                ctx.message.author.nick), icon_url="https://i.imgur.com/2VepakW.png")
+            em.title = "Purged {} messages".format(i)
+            if (ctx.message.author.nick is not None):
+                em.set_footer(text="Requested by {}".format(
+                    ctx.message.author.nick), icon_url="https://i.imgur.com/2VepakW.png")
+            else:
+                em.set_footer(text="Requested by {}".format(
+                    ctx.message.author.name), icon_url="https://i.imgur.com/2VepakW.png")
             await self.bot.say(None, embed=em)
         else:
             await self.bot.say("Purge not performed because you ain't got the power.")
