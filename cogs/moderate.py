e42a90ed6d5d33a4f5494cac6c2d8581b601fdc5
Edwan Vi <edwan.vi@gmail.com>
2018-05-18 12:14:32 -0400

    Dejank [] purge

--- cogs/moderate.py
+++ cogs/moderate.py
@@ -10,32 +10,21 @@ class moderate():
     @commands.command(description="Enables a user with Manage Messages to bulk delete the last `amount` messages.", pass_context=True)
     async def purge(self, ctx: commands.Context, amount: int):
         '''Bulk remove messages.'''
-        if (ctx.message.channel.permissions_for(ctx.message.author).manage_messages):
-            i = 0
-            j = -1
-            messages = list(self.bot.messages)
-            finish_message = None
-            while (i <= amount):
-                try:
-                    if (messages[j].channel.id == ctx.message.channel.id):
-                        await self.bot.delete_message(messages[j])
-                        messages.remove(messages[j])
-                        j = -1
-                        i += 1
-                    else:
-                        j -= 1
-                except IndexError:
-                    finish_message = "Purge aborted due to IndexError - {} tried too hard!".format(ctx.message.author.mention)
-                    break
+        channel = ctx.message.channel
+        author = ctx.message.author 
+        if (channel.permissions_for(author).manage_messages):
+            await self.bot.delete_message(ctx.message)
+            async for m in self.bot.logs_from(channel, limit=amount + 1):
+                await self.bot.delete_message(m)
             em = boiler.embed_template()
-            em.title = "Purged {} messages".format(i)
-            if (ctx.message.author.nick is not None):
+            em.title = "Purged {} messages".format(amount)
+            if (author.nick is not None):
                 em.set_footer(text="Requested by {}".format(
-                    ctx.message.author.nick), icon_url="https://i.imgur.com/2VepakW.png")
+                    author.nick), icon_url="https://i.imgur.com/2VepakW.png")
             else:
                 em.set_footer(text="Requested by {}".format(
-                    ctx.message.author.name), icon_url="https://i.imgur.com/2VepakW.png")
-            await self.bot.say(finish_message, embed=em)
+                    author.name), icon_url="https://i.imgur.com/2VepakW.png")
+            await self.bot.say(None, embed=em)
         else:
             await self.bot.say("Purge not performed because you ain't got the power.")
 
