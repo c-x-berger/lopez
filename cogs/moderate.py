825fdd5bd3a41f975f2389ea27127829e0c52368
Edwan Vi <edwan.vi@gmail.com>
2018-08-11 19:01:57 -0400

    Port remaining modules

--- cogs/moderate.py
+++ cogs/moderate.py
@@ -7,17 +7,18 @@ class moderate():
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(description="Enables a user with Manage Messages to bulk delete the last `amount` messages.", pass_context=True)
+    @commands.command(description="Enables a user with Manage Messages to bulk delete the last `amount` messages.")
     async def purge(self, ctx: commands.Context, amount: int):
         '''Bulk remove messages.'''
         channel = ctx.message.channel
         author = ctx.message.author 
         if (channel.permissions_for(author).manage_messages):
             i = 0
-            await self.bot.delete_message(ctx.message)
-            async for m in self.bot.logs_from(channel, limit=amount):
-                await self.bot.delete_message(m)
-                i += 1
+            await ctx.message.delete()
+            async with ctx.typing():
+                async for m in ctx.history(limit=amount):
+                    await m.delete()
+                    i += 1
             em = boiler.embed_template()
             em.title = "Purged {} messages".format(i)
             if (author.nick is not None):
@@ -26,9 +27,9 @@ class moderate():
             else:
                 em.set_footer(text="Requested by {}".format(
                     author.name), icon_url="https://i.imgur.com/2VepakW.png")
-            await self.bot.say(None, embed=em)
+                await ctx.send(None, embed=em, delete_after=5 if i <= 5 else None)
         else:
-            await self.bot.say("Purge not performed because you ain't got the power.")
+            await ctx.send("Purge not performed because you ain't got the power.")
 
 
 def setup(bot):
