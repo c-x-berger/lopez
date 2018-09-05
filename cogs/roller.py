3bfb8776830ccfad57cdfbf400cb25a6ef9d5e08
Edwan Vi <edwan.vi@gmail.com>
2018-09-05 11:51:57 -0400

    Add a roll ndn function + skill check command

--- cogs/roller.py
+++ cogs/roller.py
@@ -20,19 +20,40 @@ class roller():
         die_size = roller.to_int(d[1])
         return [num_roll, die_size]
 
+    @staticmethod
+    def roll_ndn(dice: list) -> list:
+        rolls = []
+        for i in range(dice[0]):
+            rolls.append(random.randint(1, dice[1]))
+        return rolls
+
     @commands.command()
     async def roll(self, ctx: commands.Context, dice: ndn):
         '''Straight rolls a die (no ability scores applied.)'''
-        rolls = []
-        for i in range(dice[0]):
-            rolls.append(random.randint(1, dice[1])) # 1 to size of dice, inclusive
+        rolls = roller.roll_ndn(dice)
         message = "**Your rolls:**\n"
-        total = 0
         for i in range(len(rolls)):
             roll = rolls[i]
-            total += roll
             message += "{} + ".format(roll) if i is not len(rolls) - 1 else "{} = ".format(roll)
-        message += "**{0!s}**".format(total)
+        message += "**{0!s}**".format(sum(rolls))
+        await ctx.send(message)
+
+    @commands.command()
+    async def skill_check(self, ctx: commands.Context, *args):
+        if len(args) == 0:
+            await ctx.send("Come back when you have things to work with!")
+            return
+        bonus = roller.to_int(args[0])
+        message = "**You rolled:**\n"
+        roll = roller.roll_ndn([1, 20])
+        total = sum(roll) + bonus
+        message += "{0} {3} {1} = **{2}**\n".format(sum(roll), abs(bonus), total, "+" if bonus >= 0 else "-")
+        try:
+            dc = roller.to_int(args[1])
+        except IndexError:
+            pass
+        else:
+            message += "{} {} {}: {}".format(total, "passes DC" if total >= dc else "fails DC", dc, "Success!" if total >= dc else "You fail!")
         await ctx.send(message)
 
 def setup(bot: commands.Bot):
