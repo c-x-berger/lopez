c9ca7eb360af8c67b446773bd69750f952693989
Edwan Vi <edwan.vi@gmail.com>
2018-09-05 18:38:02 -0400

    Start on character commands, realistically a new feature branch

--- cogs/roller.py
+++ cogs/roller.py
@@ -1,6 +1,11 @@
+import boiler
 import discord
 from discord.ext import commands
+import math
 import random
+
+scores = ["STR", "DEX", "CON", "INT", "WIS", "CHR"]
+
 
 class roller():
     def __init__(self, bot: commands.Bot):
@@ -14,6 +19,11 @@ class roller():
         except ValueError:
             return 1
 
+    @staticmethod
+    def mod_from_score(score: int) -> int:
+        return math.floor((score - 10) / 2)
+
+    @staticmethod
     def ndn(amount: str) -> list:
         d = amount.split('d')
         num_roll = roller.to_int(d[0])
@@ -23,7 +33,7 @@ class roller():
     @staticmethod
     def roll_ndn(dice: list) -> list:
         rolls = []
-        for i in range(dice[0]):
+        for _ in range(dice[0]):
             rolls.append(random.randint(1, dice[1]))
         return rolls
 
@@ -34,7 +44,8 @@ class roller():
         message = "**Your rolls:**\n"
         for i in range(len(rolls)):
             roll = rolls[i]
-            message += "{} + ".format(roll) if i is not len(rolls) - 1 else "{} = ".format(roll)
+            message += "{} + ".format(roll) if i is not len(rolls) - \
+                1 else "{} = ".format(roll)
         message += "**{0!s}**".format(sum(rolls))
         await ctx.send(message)
 
@@ -47,14 +58,38 @@ class roller():
         message = "**You rolled:**\n"
         roll = roller.roll_ndn([1, 20])
         total = sum(roll) + bonus
-        message += "{0} {3} {1} = **{2}**\n".format(sum(roll), abs(bonus), total, "+" if bonus >= 0 else "-")
+        message += "{0} {3} {1} = **{2}**\n".format(
+            sum(roll), abs(bonus), total, "+" if bonus >= 0 else "-")
         try:
             dc = roller.to_int(args[1])
         except IndexError:
             pass
         else:
-            message += "{} {} {}: {}".format(total, "passes DC" if total >= dc else "fails DC", dc, "Success!" if total >= dc else "You fail!")
+            message += "{} {} {}: {}".format(total, "passes DC" if total >=
+                                             dc else "fails DC", dc, "Success!" if total >= dc else "You fail!")
         await ctx.send(message)
+
+    @commands.group()
+    async def character(self, ctx: commands.Context):
+        if (ctx.invoked_subcommand is None):
+            await ctx.send("You need to invoke this command with a subcommand. To see available subcommands, try `[] help character`.")
+
+    @character.command(description="Creates a character with all six stats defined in a single command.\nStats MUST be in the following order: strength, dexterity, constitution, intelligence, wisdom, charisma.\nAs this is a very long command, other options are available.")
+    async def create_onecall(self, ctx: commands.Context, name: str, *stats):
+        '''Creates a character (long form.)'''
+        s = {}
+        for i in range(len(scores)):
+            stat = scores[i]
+            s[stat] = roller.to_int(stats[i])
+        em = boiler.embed_template()
+        em.title = name
+        statblock = ""
+        for stat, score in s.items():
+            modstring = "+ {}".format(roller.mod_from_score(score)) if score > 9 else "- {}".format(abs(roller.mod_from_score(score)))
+            statblock += "**{0}:** {1!s} ({2})\n".format(stat, score, modstring)
+        em.add_field(name="Stats", value=statblock)
+        await ctx.send(None, embed=em)
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(roller(bot))
