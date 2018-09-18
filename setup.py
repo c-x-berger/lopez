11ae5487c1ea81d8d7c3a06cd3abe998f6e2c997
Caleb Berger <caleb.x.berger@gmail.com>
2018-09-18 10:55:07 -0400

    Add role table to setup.py

--- setup.py
+++ setup.py
@@ -16,25 +16,40 @@ async def main():
     else:
         print('Connected!')
         print('Creating table dnd_chars...')
-        print(await conn.execute('''CREATE TABLE dnd_chars (\
-                character_id serial PRIMARY KEY,\
-                name varchar NOT NULL,\
-                strength smallint NOT NULL,\
-                dexterity smallint NOT NULL,\
-                constitution smallint NOT NULL,\
-                intelligence smallint NOT NULL,\
-                wisdom smallint NOT NULL,\
-                charisma smallint NOT NULL,\
-                race varchar NOT NULL,\
-                discord_id varchar NOT NULL,\
-                levels integer[],\
-                classes varchar[]
-            )'''))
+        try:
+            print(await conn.execute('''CREATE TABLE dnd_chars (\
+                    character_id serial PRIMARY KEY,\
+                    name varchar NOT NULL,\
+                    strength smallint NOT NULL,\
+                    dexterity smallint NOT NULL,\
+                    constitution smallint NOT NULL,\
+                    intelligence smallint NOT NULL,\
+                    wisdom smallint NOT NULL,\
+                    charisma smallint NOT NULL,\
+                    race varchar NOT NULL,\
+                    discord_id varchar NOT NULL,\
+                    levels integer[],\
+                    classes varchar[]
+                )'''))
+        except asyncpg.exceptions.DuplicateTableError:
+            print("That table already exists! If you're upgrading you can safely ignore this, if not, please drop that table and run this script again.")
+        print('Creating table role_table...')
+        print(await conn.execute('''CREATE TABLE role_table(
+            internal serial PRIMARY KEY,
+            server_id bigint NOT NULL,
+            available bigint[] NOT NULL,
+            special bigint[] NOT NULL
+        )'''))
         print('Setting unique constraints and defaults...')
+        # dnd table
         await conn.execute('''ALTER TABLE dnd_chars ADD UNIQUE (discord_id)''')
         for col in ['strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma']:
             await conn.execute('''ALTER TABLE dnd_chars ALTER COLUMN {} SET DEFAULT 0'''.format(col))
         await conn.execute("ALTER TABLE dnd_chars ALTER COLUMN levels SET DEFAULT '{1}'")
+        # role table
+        await conn.execute('''ALTER TABLE role_table ADD UNIQUE (server_id)''')
+        await conn.execute("ALTER TABLE role_table ALTER COLUMN available SET DEFAULT '{0}'")
+        await conn.execute("ALTER TABLE role_table ALTER COLUMN special SET DEFAULT '{0}'")
         print('Done!')
 
 if __name__ == '__main__':
