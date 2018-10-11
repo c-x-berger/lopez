df61fde020e27087fd228a6d2fb19b3b27b93727
Caleb Berger <caleb.x.berger@gmail.com>
2018-10-11 09:23:35 -0400

    Create boiler functions to "freeze" permissions as JSON

--- boiler.py
+++ boiler.py
@@ -5,13 +5,16 @@ Util functions. Cuts down on boilerplate code, ideally.
 import discord
 import json
 import random
+from typing import Dict
 
 quotes = []
 with open("footers.json", encoding="utf-8") as f:
     quotes = json.load(f)
 
 
-def embed_template(title: str = "Someone messed up!") -> discord.Embed:
+def embed_template(
+    title: str = "Someone messed up!", color: discord.Color = discord.Color.gold
+) -> discord.Embed:
     em = discord.Embed(title=title).set_footer(
         text=random.choice(quotes), icon_url="https://i.imgur.com/2VepakW.png"
     )
@@ -24,3 +27,29 @@ def comma_sep(values: str) -> list:
     for value in values.split(","):
         r.append(value.strip())
     return r
+
+
+def cleanup_code(content: str) -> str:
+    """Automatically removes code blocks from the code."""
+    # remove ```py\n```
+    if content.startswith("```") and content.endswith("```"):
+        return "\n".join(content.split("\n")[1:-1])
+
+    # remove `foo`
+    return content.strip("` \n")
+
+
+def perms_todict(perms: discord.PermissionOverwrite) -> Dict[str, bool]:
+    r = {}
+    for key, value in iter(perms):
+        if value is not None:
+            r[key] = value
+    return r
+
+
+def perms_tojson(perms: discord.PermissionOverwrite) -> str:
+    return json.dumps(perms_todict(perms))
+
+
+def perms_fromjson(j: str) -> discord.PermissionOverwrite:
+    return discord.PermissionOverwrite(**(json.loads(j)))
